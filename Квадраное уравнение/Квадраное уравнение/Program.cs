22b75a7302ce7232571628b6a09289b19916be14
IrinaAI <40885183+IrinaAI@users.noreply.github.com>
2018-10-16 14:15:29 +0300

    Add check of coefficients

--- Квадраное уравнение/Квадраное уравнение/Program.cs
+++ Квадраное уравнение/Квадраное уравнение/Program.cs
@@ -15,13 +15,41 @@ namespace ConsoleApplication1
             double c = double.Parse(Console.ReadLine());
             double x1, x2, D;
             D = (b * b) - (4 * a * c);
-                if (D > 0)
+            if (a != 0)
+            {
+                if (b != 0)
                 {
-                    x1 = (-b + (Math.Sqrt(D))) / (2 * a);
-                    x2 = (-b - (Math.Sqrt(D))) / (2 * a);
-                    Console.WriteLine("x1={0}, x2={1}", x1, x2);
+                    if (c != 0)
+                    {
+                        if (D != 0)
+                        {
+                            if (D > 0)
+                            {
+                                x1 = (-b + (Math.Sqrt(D))) / (2 * a);
+                                x2 = (-b - (Math.Sqrt(D))) / (2 * a);
+                                Console.WriteLine("x1={0}, x2={1}", x1, x2);
+                            }
+                            else { Console.WriteLine("Нет действительных корней"); }
+                        }
+                        else { x1 = -b / (2 * a); Console.WriteLine("x={0}", x1); }
+                    }
+                    else { x1 = 0; x2 = -b / a; Console.WriteLine("x1={0}, x2={1}", x1, x2); }
                 }
-                else { Console.WriteLine("Нет действительных корней"); }
+                else
+                {
+                    if ((-c / a) >= 0) { x1 = -Math.Sqrt(-c / a); x2 = Math.Sqrt(-c / a); Console.WriteLine("x1={0}, x2={1}", x1, x2); }
+                    else { Console.WriteLine("Нет действительных корней"); }
+                }
+            }
+            else
+            {
+                if (b != 0)
+                {
+                    x1 = -c / b;
+                    Console.WriteLine("x={0}", x1);
+                }
+                else { if (c != 0) Console.WriteLine("нет решений"); else Console.WriteLine("Х-любое"); }
+            }
             Console.ReadLine();
         }
     }
